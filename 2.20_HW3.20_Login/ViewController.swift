31083a7f38a1df75f4480663d7ef5b27fbd92ca2
Maria Masljonok <maria@onoffapp.com>
2021-05-26 10:11:58 +0300

    Initial Commit

---
+++ 2.20_HW3.20_Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.20_HW3.20_Login
+//
+//  Created by Maria Masljonok on 26.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
